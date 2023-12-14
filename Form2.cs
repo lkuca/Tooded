f8a8625a65d4c53f67562e4b57667fc81ec496a1
lkuca <120181604+lkuca@users.noreply.github.com>
2023-12-14 10:00:40 +0200

    Add files via upload

--- Form2.cs
+++ Form2.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Tooded
 {
@@ -16,7 +17,8 @@ namespace Tooded
     {
         public Form2()
         {
-            
+            List<string> logins = new List<string>();
+            List<string> passwords = new List<string>();
             string[] log = { "User1", "User2" };
             string[] parool = { "s1mple", "juppi" };
             this.Height = 400;
@@ -60,6 +62,31 @@ namespace Tooded
 
 
         }
+        private void tekst_to_lbl(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Entered text: " + text);
+        }
+        public static bool salsasona(int k, out string password)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < k; i++)
+            {
+                char t = (char)random.Next('A', 'Z' + 1);
+                int num = random.Next(0, 10);
+                char sym = random.//Char(new[] { '*', '-', '.', '!', '_' });
+
+                char[] t_num = { t, num.ToString()[0], sym };
+                sb.Append(t_num[random.Next(0, 3)]);
+            }
+
+            password = sb.ToString();
+            return true;
+        }
 
         private void registerimine_nimi(object sender, EventArgs e)
         {
@@ -71,20 +98,20 @@ namespace Tooded
 
             label_nickname.Text = "Enter your nickname:";
             label_nickname.BackColor = System.Drawing.Color.Silver;
-            label_nickname.ForeColor = System.Drawing.Color.FromArgb(170, 74, 68);
-            label_nickname.Font = new System.Drawing.Font("Arial", 20);
-            label_nickname.Height = 1;
-            label_nickname.Width = 40;
-            label_nickname.Location = new Point(0,0);
+            
+            
+            label_nickname.Height = 22;
+            label_nickname.Width = 146;
+            label_nickname.Location = new Point(240, 190);
 
             nicknameTextBox.ForeColor = System.Drawing.Color.Blue;
             nicknameTextBox.Text = "Enter your nickname:";
             nicknameTextBox.BackColor = System.Drawing.Color.LightBlue;
             nicknameTextBox.Height = 1;
-            nicknameTextBox.Width = 40;
-            nicknameTextBox.Font = new System.Drawing.Font("Arial", 20);
+            nicknameTextBox.Width = 146;
+            
             nicknameTextBox.TextAlign = HorizontalAlignment.Center;
-            nicknameTextBox.Location = new Point(10,label_nickname.Bottom);
+            nicknameTextBox.Location = new Point(244,217);
             aken1.Controls.Add(label_nickname);
             aken1.Controls.Add(nicknameTextBox);
             aken1.Show();
